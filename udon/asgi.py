59091f3160dee88f2bfb69597cf2138ee9116a5e
Romain Petit <romain@omnibook.com>
2025-01-27 10:43:51 +0100

    wip: asgi

---
+++ udon/asgi.py
@@ -0,0 +1,401 @@
+import datetime
+import email.utils
+import importlib
+import logging
+import os.path
+import time
+import traceback
+
+import fastapi
+from fastapi import Depends
+import fastapi.responses
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
+import uvicorn
+
+import udon.util
+
+
+_apis = {}
+def api(path):
+    def _(setup):
+        _apis[setup] = API(path, setup)
+    return _
+
+
+class API:
+
+    stack = None
+    app = None
+
+    def __init__(self, prefix, setup):
+        self.prefix = prefix
+        self.setup = setup
+
+    def install(self, stack):
+        assert self.stack is None
+        mount_point = os.path.join(stack.prefix, self.prefix)
+        if not mount_point.endswith('/'):
+            mount_point += '/'
+        app = stack.app_factory()
+        self.setup(app)
+        stack.app.mount(mount_point, self)
+        self.app = app
+        self.stack = stack
+
+    async def __call__(self, scope, receive = None, send = None):
+        return await self.app(scope, receive, send)
+
+
+class APIStack:
+
+    def __init__(self, prefix = "/", **options):
+        self.prefix = prefix
+        self.options = options
+        self.app = self.app_factory()
+        self.apis = []
+
+    def app_factory(self):
+        return App(**self.options)
+
+    def install_func(self, prefix, setup):
+        api = API(prefix, setup)
+        api.install(self)
+        self.apis.append(api)
+
+    def install_module(self, module_name):
+        importlib.import_module(module_name)
+        for api in _apis.values():
+            if not api.stack:
+                api.install(self)
+
+    def on_startup(self, method):
+        self.app.on_startup(method)
+
+    def on_shutdown(self, method):
+        self.app.on_shutdown(method)
+
+    def add_middleware(self, cls, **args):
+        self.app.add_middleware(cls, **args)
+        for api in self.apis:
+            api.app.add_middleware(cls, **args)
+
+    def add_http_middleware(self, method, **args):
+        self.app.add_http_middleware(method, **args)
+        for api in self.apis:
+            api.app.add_http_middleware(method, **args)
+
+    async def __call__(self, scope, receive = None, send = None):
+        return await self.app(scope, receive, send)
+
+
+class App(fastapi.FastAPI):
+    def get(self, path, method, **args):
+        fastapi.FastAPI.get(self, path, **args)(method)
+
+    def post(self, path, method, **args):
+        fastapi.FastAPI.post(self, path, **args)(method)
+
+    def put(self, path, method, **args):
+        fastapi.FastAPI.put(self, path, **args)(method)
+
+    def patch(self, path, method, **args):
+        fastapi.FastAPI.patch(self, path, **args)(method)
+
+    def head(self, path, method, **args):
+        fastapi.FastAPI.head(self, path, **args)(method)
+
+    def delete(self, path, method, **args):
+        fastapi.FastAPI.delete(self, path, **args)(method)
+
+    def options(self, path, method, **args):
+        fastapi.FastAPI.options(self, path, **args)(method)
+
+    def on_startup(self, method):
+        fastapi.FastAPI.on_event(self, "startup")(method)
+
+    def on_shutdown(self, method):
+        fastapi.FastAPI.on_event(self, "shutdown")(method)
+
+    # def add_middleware(self, cls, **args):
+    #     self.add_middleware(cls, **args)
+
+    def add_http_middleware(self, method, **args):
+        fastapi.FastAPI.middleware(self, "http")(method, **args)
+
+    # TODO: follow asgi best practice for error handling
+    async def __call__(self, scope: Scope, receive = Receive, send = Send):
+        try:
+            return await fastapi.FastAPI.__call__(self, scope, receive, send)
+        except Exception as exc:
+            logging.error(format_exception(exc))
+            raise
+
+
+def format_exception(exc):
+    """
+    Format exception to string
+    """
+    return "".join(traceback.format_exception(type(exc), exc, exc.__traceback__))
+
+
+def run(app, **args):
+    try:
+        uvicorn.run(app, **args)
+    except SystemExit:
+        pass
+    except:  # noqa: E722
+        logging.exception("EXCEPTION")
+
+
+async def request_to_stream_generator(req):
+    # TODO: test/understand behavior of iter_* methods.
+    # Eventually fallback to iter_chunked with a good chunk value ?
+    async for data in req.content.iter_any():
+        yield data
+
+
+async def log_http_middleware(request: fastapi.Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+
+    try:
+        length = str(len(response.content))
+    except:
+        length = "-"
+
+    logging.info(
+        "%.3f %s %s %s %d %s %s" % (
+            process_time,
+            request.client.host,
+            request.headers.get("HTTP_X_FORWARDED_FOR", "-"),
+            request.method,
+            response.status_code,
+            length,
+            request.url
+        )
+    )
+
+    return response
+
+
+class LogMiddleware:
+    def __init__(self, app: ASGIApp, *args, **kwargs):
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        start_time = time.perf_counter()
+        await self.app(scope, receive, send)
+        process_time = time.perf_counter() - start_time
+        length = "-"
+
+        logging.debug(scope)
+        return
+
+        logging.info(
+            "%.3f %s %s %s %d %s %s" % (
+                process_time,
+                ":".join(scope.client),
+                request.headers.get("HTTP_X_FORWARDED_FOR", "-"),
+                request.method,
+                response.status_code,
+                length,
+                request.url
+            )
+        )
+
+
+def streaming(request: fastapi.Request, size: int, fd, timestamp: int = None, etag: str = None, headers: dict = None):
+    status = 200
+    body = iter(())
+    length = size
+    range_ = parse_range(request, length)
+    headers = {} if headers is None else headers
+
+    if not none_match(etag, request) or not modified_since(timestamp, request):
+        length = 0
+        status = 304
+    elif range_:
+        offset, end = range_
+        length = end - offset
+        headers["Content-Range"] = "bytes %d-%d/%d" % (offset, end - 1, size)
+        status = 206
+        body = iter_range(fd, offset, end - offset, None)
+    elif request.method != "HEAD":
+        body = fd
+
+    return fastapi.responses.StreamingResponse(
+        content=body,
+        status_code=status,
+        # Caching logic here is to support both If-None-Match and If-Modified-Since
+        # invalidation mechanism to allow a "small" caching age of one hour.
+        headers = {
+            "Cache-Control": "public, max-age=3600",
+            "Content-Length": str(length),
+            "Last-Modified": formatdate(timestamp),
+            "Accept-Ranges": "bytes",
+            # An Etag containing the mtime is pretty much useless, should be a real file hash.
+            # TODO: use info.CRC from ZipInfo, and swift object MD5
+            "Etag": etag,
+            "Server": "Omnibook (uvicorn)",
+            **headers})
+
+
+def not_found(request: fastapi.Request):
+    # Get not found page
+    locale = find_locale(request)
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files/errors/404.%s.html" % locale)
+    with open(path, "r") as fp:
+        content = fp.read()
+
+    # Change home link to current website root (for websites)
+    home_link = "%s://%s" % (request.url.scheme, request.url.hostname)
+    content = content.replace('href="https://omnibook.com"', 'href="%s"' % home_link)
+
+    return fastapi.responses.HTMLResponse(content=content,
+                                          status_code=404,
+                                          headers={"Cache-Control": "public, max-age=60"})
+
+
+def find_locale(request: fastapi.Request):
+    return udon.util.find_locale(request.headers.get("Accept-Language", ""))
+
+
+def parse_range(request: fastapi.Request, size):
+    value = request.headers.get("Range", "")
+    if not value.startswith("bytes="):
+        return None
+
+    for rng in value.split("=", 1)[1].split(","):
+        if "-" not in rng:
+            continue
+        offset, end = rng.split("-", 1)
+        if (offset, end) == ("", ""):
+            continue
+        if not offset:
+            offset, end = max(0, size - int(end) + 1), size
+        elif not end:
+            offset, end = int(offset), size
+        else:
+            offset, end = int(offset), int(end) + 1
+        if 0 <= offset < end <= size:
+            return offset, end
+
+    return None
+
+
+def iter_range(body, offset, count, logger):
+    try:
+        if (offset):
+            if body.seekable():
+                body.seek(offset, 1)
+            else:
+                for _ in iter_range_chunk(body, offset):
+                    pass
+        for chunk in iter_range_chunk(body, count):
+            yield chunk
+    except GeneratorExit:
+        # interrupted transfer
+        pass
+    finally:
+        body.close()
+
+
+def iter_range_chunk(fp, count, bufsize = 1024 * 1024):
+    while count:
+        data = fp.read(min(count, bufsize))
+        if not data:
+            break
+        yield data
+        count -= len(data)
+
+
+def client_ip(request):
+    return request.client.host or request.headers.get("x-forwarded-for")
+
+
+def formatdate(ts):
+    """ Format an HTTP-Date """
+    return email.utils.formatdate(timeval = ts, usegmt = True)
+
+
+def parsedate(date: str):
+    """ Parse an HTTP-Date into a datetime """
+    return email.utils.parsedate_to_datetime(date)
+
+
+def modified_since(ts: int, request: fastapi.Request):
+    ims = request.headers.get("If-Modified-Since", "")
+    if ims:
+        # mdate is offset-naive, must make imsdate naive too
+        mdate = datetime.datetime.utcfromtimestamp(ts)
+        imsdate = parsedate(ims).replace(tzinfo = None)
+
+        return (imsdate < mdate)
+
+    return True
+
+
+def none_match(etag: str, request: fastapi.Request):
+    """
+    etag: must already be enclosed with double quotes
+    """
+    # Get clean header
+    inm = request.headers.get("If-None-Match", "").strip(" ")
+    if inm:
+        # Remove leading weak marker if needed
+        if inm.startswith("W/"):
+            inm = inm[2:]
+        # List all etags, each double-quote encosed and coma separated
+        etags = [part.strip(" ") for part in inm.split(",")]
+
+        return (etag not in etags)
+
+    return True
+
+
+def access_control_route_handler(method, cors):
+    options = {} if cors is True else cors
+
+    def _(request: fastapi.Request, response: fastapi.Response):
+        set_access_control_headers(response,
+                                   method="GET",
+                                   origin=options.get("origin", request.headers.get("Origin")),
+                                   headers=options.get("headers", []))
+    return _
+
+
+def access_control_options_handler(method, cors):
+    options = {} if cors is True else cors
+
+    def _(request: fastapi.Request, response: fastapi.Response):
+        set_access_control_headers(response,
+                                   method=method,
+                                   origin=options.get("origin", request.headers.get("Origin")),
+                                   headers=options.get("headers", []))
+    return _
+
+
+def set_access_control_headers(response, method = "GET", origin = None, headers = []):
+    for key, value in access_control_headers(method, origin, headers).items():
+        response.headers[key] = value
+
+    return response
+
+
+def access_control_headers(method = "GET", origin = None, headers = []):
+    return {
+        "Access-Control-Allow-Origin": origin or "*",
+        "Access-Control-Allow-Methods": method,
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Headers": ", ".join(headers),
+        "Access-Control-Max-Age": "600",
+        # 1. The response will obviously vary with Origin because of the dynamic "Access-Control-Allow-Origin" header.
+        # This is required so the CORS is not cached by browser for example when switching between editor and preview.
+        # editor uses the main origin (ex: https://omnibook.com) and preview uses the subdomain origin (ex: https://<uuid>.omnibook.com)
+        # and resources loaded cross-origin (ex: fonts from the theme) must not use the cached response from different origin
+        # or the UA will emit a CORS error and the font wont load.
+        # 2. The "Accept-Encoding" vary header is automatically added by haproxy but it seems that setting the Vary header
+        # prevents haproxy to put his version, so put both here to prevent invalid responses.
+        "Vary": "Origin, Accept-Encoding",
+    }
--- udon/util.py
+++ udon/util.py
@@ -19,3 +19,38 @@ class nullcontext:
         pass
     def __exit__(self, _type, _value, _traceback):
         pass
+
+
+def find_locale(header: str, default: str = "en"):
+    languages = parse_access_language(header)
+    for language in languages:
+        if language["locale"] in Locales:
+            return language["locale"]
+    return default
+
+
+def default_accept_language(header):
+    if header:
+        languages = parse_access_language(header)
+        if languages:
+            return languages[0]["locale"]
+    return None
+
+
+def parse_access_language(header):
+    """
+    Parses the Access-Language header
+    ex: "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,it;q=0.6,th;q=0.5,zh-CN;q=0.4,zh;q=0.3,nb;q=0.2,et;q=0.1,es;q=0.1"
+    """
+    languages = []
+    for level in header.split(","):
+        language = {"locale": "",
+                    "q": "1"}
+        for index, part in enumerate(level.strip().split(";")):
+            value = part.split("=")
+            if not index:
+                language["locale"] = part
+            elif len(value) == 2:
+                language[value[0]] = value[1]
+        languages.append(language)
+    return languages
